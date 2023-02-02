a7c8077bc71434feffe2e42a2db85f4e47e14c96
Ashnaali3255 <ashnaalipk@gmail.com>
2023-02-02 13:54:35 +0500

    Create js linters and update js files

--- js/script.js
+++ js/script.js
@@ -1,102 +1,102 @@
 const array = [
-    {
-      id: '1',
-      name: 'Tonic',
-      namedesk: 'Tonic',
-      shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-      shortDescriptiondesk: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-      longDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+  {
+    id: '1',
+    name: 'Tonic',
+    namedesk: 'Tonic',
+    shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    shortDescriptiondesk: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    longDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
       industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
       scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
       industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
       printer took a galley of type and scrambled it 1960s with the relea`,
-      featured: ['CANOPY', 'Back End Dev', 2023],
-      featureddesk: ['CANOPY', 'Back End Dev', 2023],
-      image: './First-image.jpg',
-      imageDesktop: './images/desktop/projectone.svg',
-      technologies: ['html', 'css', 'javaScript'],
-      technologiesdesk: ['html', 'css', 'javaScript', 'Ruby on rails;'],
-      live_version: 'https://ashnaali3255.github.io/Portfolio-2/',
-      source_link: 'https://github.com/Ashnaali3255/Portfolio-2',
-    },
-    {
-      id: '2',
-      name: 'Multi-Post Stories',
-      namedesk: 'Multi-Post Stories',
-      shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-      shortDescriptiondesk: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-      longDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+    featured: ['CANOPY', 'Back End Dev', 2023],
+    featureddesk: ['CANOPY', 'Back End Dev', 2023],
+    image: './First-image.jpg',
+    imageDesktop: './images/desktop/projectone.svg',
+    technologies: ['html', 'css', 'javaScript'],
+    technologiesdesk: ['html', 'css', 'javaScript', 'Ruby on rails;'],
+    live_version: 'https://ashnaali3255.github.io/Portfolio-2/',
+    source_link: 'https://github.com/Ashnaali3255/Portfolio-2',
+  },
+  {
+    id: '2',
+    name: 'Multi-Post Stories',
+    namedesk: 'Multi-Post Stories',
+    shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    shortDescriptiondesk: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    longDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
       industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
       scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
       industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
       printer took a galley of type and scrambled it 1960s with the relea`,
-      featured: ['CANOPY', 'Back End Dev', 2023],
-      featureddesk: ['FACEBOOK', 'Full Stack Dev', 2023],
-      image: './Second-image.jpg',
-      imageDesktop: './images/desktop/projecttwo.svg',
-      technologies: ['html', 'css', 'javaScript'],
-      technologiesdesk: ['html', 'css', 'javaScript', 'Ruby on rails;'],
-      live_version: 'https://ashnaali3255.github.io/Portfolio-2/',
-      source_link: 'https://github.com/Ashnaali3255/Portfolio-2',
-    },
-    {
-      id: '3',
-      name: 'Tonic',
-      namedesk: 'Facebook 360',
-      shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-      shortDescriptiondesk: 'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-      longDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+    featured: ['CANOPY', 'Back End Dev', 2023],
+    featureddesk: ['FACEBOOK', 'Full Stack Dev', 2023],
+    image: './Second-image.jpg',
+    imageDesktop: './images/desktop/projecttwo.svg',
+    technologies: ['html', 'css', 'javaScript'],
+    technologiesdesk: ['html', 'css', 'javaScript', 'Ruby on rails;'],
+    live_version: 'https://ashnaali3255.github.io/Portfolio-2/',
+    source_link: 'https://github.com/Ashnaali3255/Portfolio-2',
+  },
+  {
+    id: '3',
+    name: 'Tonic',
+    namedesk: 'Facebook 360',
+    shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    shortDescriptiondesk: 'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    longDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
       industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
       scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
       industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
       printer took a galley of type and scrambled it 1960s with the relea`,
-      featured: ['CANOPY', 'Back End Dev', 2023],
-      featureddesk: ['FACEBOOK', 'Full Stack Dev', 2023],
-      image: 'Third-image.jpg',
-      imageDesktop: './images/desktop/projectthree.svg',
-      technologies: ['html', 'css', 'javaScript'],
-      technologiesdesk: ['html', 'css', 'javaScript', 'Ruby on rails;'],
-      live_version: 'https://ashnaali3255.github.io/Portfolio-2/',
-      source_link: 'https://github.com/Ashnaali3255/Portfolio-2',
-    },
-    {
-      id: '4',
-      name: 'Multi-Post Stories',
-      namedesk: 'Uber Navigation',
-      shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-      shortDescriptiondesk: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-      longDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+    featured: ['CANOPY', 'Back End Dev', 2023],
+    featureddesk: ['FACEBOOK', 'Full Stack Dev', 2023],
+    image: 'Third-image.jpg',
+    imageDesktop: './images/desktop/projectthree.svg',
+    technologies: ['html', 'css', 'javaScript'],
+    technologiesdesk: ['html', 'css', 'javaScript', 'Ruby on rails;'],
+    live_version: 'https://ashnaali3255.github.io/Portfolio-2/',
+    source_link: 'https://github.com/Ashnaali3255/Portfolio-2',
+  },
+  {
+    id: '4',
+    name: 'Multi-Post Stories',
+    namedesk: 'Uber Navigation',
+    shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    shortDescriptiondesk: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    longDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
       industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
       scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
       industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
       printer took a galley of type and scrambled it 1960s with the relea`,
-      featured: ['CANOPY', 'Back End Dev', 2023],
-      featureddesk: ['UBER', 'Lead Developer', 2023],
-      image: './Fourth-image.jpg',
-      imageDesktop: './images/desktop/projectfour.svg',
-      technologies: ['html', 'css', 'javaScript'],
-      technologiesdesk: ['html', 'css', 'javaScript', 'Ruby on rails;'],
-      live_version: 'https://ashnaali3255.github.io/Portfolio-2/',
-      source_link: 'https://github.com/Ashnaali3255/Portfolio-2',
-    },
-  ];
-  const main = document.querySelector('.menu-icon');
-  const dynamicWork = document.querySelector('.projects');
-  const modalView = document.querySelector('.bg-modal');
-  const openMain = () => {
-    main.style.visibility = 'visible';
-  };
-  const closeMain = () => {
-    main.style.visibility = 'hidden';
-  };
-  const closeModal = () => {
-    modalView.style.visibility = 'hidden';
-  };
-  const openModal = (id) => {
-    modalView.style.visibility = 'visible';
-    for (let i = 0; i < array.length; i += 1) {
-      if (id === array[i].id) {
-        modalView.innerHTML = `
+    featured: ['CANOPY', 'Back End Dev', 2023],
+    featureddesk: ['UBER', 'Lead Developer', 2023],
+    image: './Fourth-image.jpg',
+    imageDesktop: './images/desktop/projectfour.svg',
+    technologies: ['html', 'css', 'javaScript'],
+    technologiesdesk: ['html', 'css', 'javaScript', 'Ruby on rails;'],
+    live_version: 'https://ashnaali3255.github.io/Portfolio-2/',
+    source_link: 'https://github.com/Ashnaali3255/Portfolio-2',
+  },
+];
+const main = document.querySelector('.menu-icon');
+const dynamicWork = document.querySelector('.projects');
+const modalView = document.querySelector('.bg-modal');
+const openMain = () => {
+  main.style.visibility = 'visible';
+};
+const closeMain = () => {
+  main.style.visibility = 'hidden';
+};
+const closeModal = () => {
+  modalView.style.visibility = 'hidden';
+};
+const openModal = (id) => {
+  modalView.style.visibility = 'visible';
+  for (let i = 0; i < array.length; i += 1) {
+    if (id === array[i].id) {
+      modalView.innerHTML = `
         <div class="model_content" id="modelcontent">
         <div class="model_header">
             <h2 class="project_title">${array[i].name}</h2>
@@ -123,15 +123,15 @@ const array = [
         </div>
         </div>
         </div>`;
-      }
     }
-  };
-  openMain();
-  closeMain();
-  openModal();
-  closeModal();
-  for (let i = 0; i < array.length; i += 1) {
-    dynamicWork.innerHTML += `<div class="project">
+  }
+};
+openMain();
+closeMain();
+openModal();
+closeModal();
+for (let i = 0; i < array.length; i += 1) {
+  dynamicWork.innerHTML += `<div class="project">
       <img class="picture" src="${array[i].image}" alt="project1">
       <h2 class="post">${array[i].name}</h2>
       <ul class="toni-flex">
@@ -176,4 +176,4 @@ const array = [
   </div>
   </div>
     `;
-  }
+}
